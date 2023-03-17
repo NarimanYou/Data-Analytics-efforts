dba96a94f0af87ec26205f3da5f9a0ebbd0fd53c
Nariman Yousefzadeh <126755476+NarimanYou@users.noreply.github.com>
2023-03-17 00:57:59 -0400

    Add files via upload

---
+++ Python_Basic_2/my_module.py
@@ -0,0 +1,12 @@
+
+
+#my_module.py
+
+def calculate_square_footage(length, width):
+    return length * width
+
+def calculate_circumference(radius):
+    return 2 * 3.14 * radius
+
+def feet_to_inches(feet):
+    return feet * 12
